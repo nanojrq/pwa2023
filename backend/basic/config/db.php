be0e88eb7425a9bfda1915a9e76b640d2513bf72
unknown <nanojrq@gmail.com>
2023-09-20 17:01:03 -0300

    yii + conexion bbdd

---
+++ backend/basic/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host='.$_ENV['DDBB_HOST'].';dbname='.$_ENV['DDBB_NAME'].'',
+    'username' => 'root',
+    'password' => $_ENV['DDBB_PASSWORD'],
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
