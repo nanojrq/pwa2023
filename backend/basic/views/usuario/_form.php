15612ab4e1cb904997ec47672c52ff6769e28325
unknown <nanojrq@gmail.com>
2023-09-20 18:45:24 -0300

    punto 4

--- backend/basic/views/usuario/_form.php
+++ backend/basic/views/usuario/_form.php
@@ -18,9 +18,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'authkey')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'accesstoken')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
